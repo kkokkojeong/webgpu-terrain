1e9a07d9e1a54db358973ce7e8eb5d143c5004a7
kkokkojeong <comgraphics11@gmail.com>
2023-09-18 16:23:15 +0900

    first terrain skeleton codes

---
+++ src/shaders/basic.ts
@@ -0,0 +1,22 @@
+const vs = `
+    @vertex
+    fn main(@builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4f  {
+        let pos = array(
+            vec2f( 0.0,  0.5),  // top center
+            vec2f(-0.5, -0.5),  // bottom left
+            vec2f( 0.5, -0.5)   // bottom right
+        );
+        return vec4f(pos[vertexIndex], 0.0, 1.0);
+    }
+`;
+const fs = `
+    @fragment
+    fn main() -> @location(0) vec4f {
+        return vec4f(1, 0, 0, 1);
+    }
+`;
+
+export default {
+    vertexShader: vs,
+    fragmentShader: fs
+}
